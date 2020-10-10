2bf4a8937347708e560509da6da41186ad0860ec
fakelag <35497506+fakelag@users.noreply.github.com>
2020-10-10 22:32:19 +0300

    Added functions and function calls

--- turbine-lang/Main.cpp
+++ turbine-lang/Main.cpp
@@ -8,9 +8,9 @@
 /*
 	Fn FuncName arg0, arg1:
 		While cond:
-			Call FuncName;
-			Call FuncName 1;
-			Call FuncName (1, 2, 3);
+			FuncName()
+			FuncName(1);
+			FuncName(1, 2, 3);
 		End While
 	End Fn
 
@@ -22,6 +22,25 @@
 #define encode_double_1( dbl ) (uint32_t)(( *(uint64_t*)(&dbl) & 0xFFFFFFFFLL));
 #define decode_double( target, part0, part1 ) (*reinterpret_cast<uint64_t*>(&target) = ((uint64_t)part0 << 32 | part1));
 
+enum FunctionType {
+	fn_global,
+	fn_main,
+	fn_virtual,
+};
+
+struct Function {
+	std::string								name;
+	std::vector< uint32_t >					code;
+	int										index;
+	FunctionType							type;
+};
+
+struct Program {
+	int										global;
+	int										main;
+	std::vector< Function >					functions;
+};
+
 enum OpCode : uint32_t {
 	op_add,
 	op_sub,
@@ -32,6 +51,7 @@ enum OpCode : uint32_t {
 	op_load_slot,
 	op_pop,
 	op_return,
+	op_call,
 };
 
 enum TokenType {
@@ -200,40 +220,66 @@ std::vector< Token > tokenize( const std::string& input ) {
 struct Parser {
 	struct Slot {
 		int							depth;
-		int							index;
+		int							local_index;
+		int							slot_index;
 		bool						is_defined;
 		std::string					name;
 	};
 
-	std::vector< Token >					tokens;
-	std::vector< Token >::const_iterator	iterator;
+	std::vector< Token >						tokens;
+	std::vector< Token >::const_iterator		token_iterator;
 
-	std::vector< uint32_t >*				code;
-	std::vector< Slot >						stack;
-	int										stack_depth;
+	std::vector< Function >						functions;
+	int											current_function;
+
+	std::vector< Slot >							stack;
+	int											stack_depth;
 };
 
 void parse_precedence( Parser& parser, int rbp = 0 );
 void parse_number( Parser& parser );
+void statement( Parser& parser );
+void expression( Parser& parser );
 
 void emit( Parser& parser, uint32_t byte ) {
-	parser.code->push_back( byte );
+	parser.functions[ parser.current_function ].code.push_back( byte );
 }
 
 const Token& advance_token( Parser& parser ) {
-	return *parser.iterator++;
+	return *parser.token_iterator++;
 }
 
 const Token& get_current_token( Parser& parser ) {
-	return *parser.iterator;
+	return *parser.token_iterator;
 }
 
-const Token& get_previous_token( Parser& parser ) {
-	return *( parser.iterator - 1 );
+const Token& get_previous_token( Parser& parser, int offset = 0 ) {
+	return *( parser.token_iterator - 1 - offset );
+}
+
+int get_local_index_for_depth( Parser& parser, int depth = 0 ) {
+	int local_index = -1;
+	for ( auto it = parser.stack.rbegin(); it != parser.stack.rend(); ++it ) {
+		if ( it->depth == depth ) {
+			local_index = it->local_index;
+			break;
+		}
+	}
+
+	return local_index;
 }
 
 const Parser::Slot& create_variable( Parser& parser, const std::string& name ) {
-	parser.stack.push_back( Parser::Slot{ parser.stack_depth, ( int ) parser.stack.size(), false, name } );
+	parser.stack.push_back(
+		Parser::Slot{
+			parser.stack_depth,
+			get_local_index_for_depth( parser, parser.stack_depth ) + 1,
+			( int ) parser.stack.size(),
+			false,
+			name,
+		}
+	);
+
 	return parser.stack[ parser.stack.size() - 1 ];
 }
 
@@ -254,6 +300,27 @@ void destroy_scope( Parser& parser ) {
 	--parser.stack_depth;
 }
 
+void create_function( Parser& parser, const std::string& name, FunctionType type = FunctionType::fn_virtual ) {
+	parser.functions.push_back( Function{ name, {}, ( int ) parser.functions.size(), type } );
+	create_scope( parser );
+
+	parser.current_function = parser.functions.size() - 1;
+
+	printf( ">> Compiling function: %s\n", name.c_str() );
+}
+
+void finish_function( Parser& parser ) {
+	emit( parser, OpCode::op_load_zero );
+	emit( parser, OpCode::op_return );
+	printf( "emit op_load_zero\n" );
+	printf( "emit op_return\n" );
+
+	printf( "<< Finished function: %s\n", parser.functions[ parser.current_function ].name.c_str() );
+
+	destroy_scope( parser );
+	parser.current_function = 0;
+}
+
 bool find_variable( Parser& parser, const std::string& name, Parser::Slot* target_slot ) {
 	auto find_result = std::find_if( parser.stack.begin(), parser.stack.end(), [ &name ]( const Parser::Slot& slot ) {
 		return slot.name == name;
@@ -267,12 +334,25 @@ bool find_variable( Parser& parser, const std::string& name, Parser::Slot* targe
 	return true;
 }
 
-void define_variable( Parser& parser, int slot ) {
-	parser.stack[ slot ].is_defined = true;
+bool find_function( Parser& parser, const std::string& name, int* function_index ) {
+	auto find_result = std::find_if( parser.functions.begin(), parser.functions.end(), [ &name ]( const Function& slot ) {
+		return slot.name == name;
+	} );
+
+	if ( find_result == parser.functions.end() ) {
+		return false;
+	}
+
+	*function_index = std::distance( parser.functions.begin(), find_result );
+	return true;
+}
+
+void define_variable( Parser& parser, int slot_index ) {
+	parser.stack[ slot_index ].is_defined = true;
 }
 
 void expect( Parser& parser, TokenType token, const std::string& error ) {
-	if ( parser.iterator->token_type == token ) {
+	if ( parser.token_iterator->token_type == token ) {
 		advance_token( parser );
 		return;
 	}
@@ -281,7 +361,7 @@ void expect( Parser& parser, TokenType token, const std::string& error ) {
 }
 
 bool match( Parser& parser, TokenType token ) {
-	if ( parser.iterator->token_type == token ) {
+	if ( parser.token_iterator->token_type == token ) {
 		advance_token( parser );
 		return true;
 	}
@@ -290,7 +370,7 @@ bool match( Parser& parser, TokenType token ) {
 }
 
 bool is_finished( Parser& parser ) {
-	return parser.iterator == parser.tokens.end();
+	return parser.token_iterator == parser.tokens.end();
 }
 
 void emit_load_number( Parser& parser, double number ) {
@@ -329,16 +409,55 @@ void parse_identifier( Parser& parser ) {
 	auto identifier_token = get_previous_token( parser );
 
 	Parser::Slot slot;
+	int function_index;
+
 	if ( find_variable( parser, identifier_token.token_string, &slot ) ) {
 		if ( !slot.is_defined ) {
 			throw std::exception( ( "Can not refer to identifier '" + slot.name + "' before it is initialized" ).c_str() );
 		}
 
 		emit( parser, OpCode::op_load_slot );
-		emit( parser, slot.index );
-		printf( "emit op_load_slot [%i]\n", slot.index );
+		emit( parser, slot.local_index );
+		printf( "emit op_load_slot [%i]\n", slot.local_index );
+	} else if ( find_function( parser, identifier_token.token_string, &function_index ) ) {
+		// No-op
 	} else {
 		throw std::exception( ( "Identifier '" + identifier_token.token_string + "' not found" ).c_str() );
+	}
+}
+
+void parse_call( Parser& parser ) {
+	auto identifier_token = get_previous_token( parser, 1 );
+
+	if ( identifier_token.token_type != TokenType::token_identifier ) {
+		throw std::exception( ( "Expected an identifier, got '" + identifier_token.token_string + "'" ).c_str() );
+	}
+
+	int function_index;
+	if ( !find_function( parser, identifier_token.token_string, &function_index ) ) {
+		throw std::exception( ( "Identifier '" + identifier_token.token_string + "' not found" ).c_str() );
+	}
+
+	if ( match( parser, TokenType::token_paren_right ) ) {
+		emit( parser, OpCode::op_call );
+		emit( parser, function_index );
+		emit( parser, 0 );
+
+		printf( "emit op_call [%i, 0]\n", function_index );
+	} else {
+		int arg_count = 0;
+
+		do {
+			++arg_count;
+			expression( parser );
+		} while ( match( parser, TokenType::token_comma ) );
+
+		emit( parser, OpCode::op_call );
+		emit( parser, function_index );
+		emit( parser, arg_count );
+
+		printf( "emit op_call [%i, %i]\n", function_index, arg_count );
+		expect( parser, TokenType::token_paren_right, "Expected ')' after argument list" );
 	}
 }
 
@@ -361,6 +480,9 @@ void parse_precedence( Parser& parser, int rbp ) {
 		case TokenType::token_star:
 		case TokenType::token_slash:
 			parse_binary( parser );
+			break;
+		case TokenType::token_paren_left:
+			parse_call( parser );
 			break;
 		default:
 			throw std::exception( "Expected a binary operator" );
@@ -385,9 +507,34 @@ void const_declaration( Parser& parser ) {
 		printf( "emit op_load_zero\n" );
 	}
 
+	define_variable( parser, slot.slot_index );
 	expect( parser, TokenType::token_semicolon, "Expected ';' after constant declaration" );
+}
 
-	define_variable( parser, slot.index );
+void function_declaration( Parser& parser ) {
+	expect( parser, TokenType::token_identifier, "Expected identifier after 'Fn'" );
+
+	auto identifier_token = get_previous_token( parser );
+	create_function( parser, identifier_token.token_string, FunctionType::fn_virtual );
+
+	if ( !match( parser, TokenType::token_colon ) ) {
+		do {
+			expect( parser, TokenType::token_identifier, "Expected identifier or ':'" );
+			auto arg_identifier = get_previous_token( parser );
+
+			auto arg_variable = create_variable( parser, arg_identifier.token_string );
+			define_variable( parser, arg_variable.slot_index );
+		} while ( match( parser, TokenType::token_comma ) );
+
+		expect( parser, TokenType::token_colon, "Expected ':' after argument list" );
+	}
+
+	while ( !match( parser, token_end ) ) {
+		statement( parser );
+	}
+
+	finish_function( parser );
+	expect( parser, TokenType::token_function, "Expected 'Fn' after 'End'" );
 }
 
 void return_statement( Parser& parser ) {
@@ -418,61 +565,86 @@ void statement( Parser& parser ) {
 void declaration( Parser& parser ) {
 	if ( match( parser, TokenType::token_const ) ) {
 		const_declaration( parser );
+	}  else if ( match( parser, TokenType::token_function ) ) {
+		function_declaration( parser );
 	} else {
 		statement( parser );
 	}
 }
 
-bool parse( const std::vector< Token >& tokens, std::vector< uint32_t >* code ) {
+void parse( const std::vector< Token >& tokens, Program* program ) {
 	Parser parser;
 
 	parser.tokens = tokens;
-	parser.iterator = parser.tokens.begin();
-	parser.code = code;
+	parser.token_iterator = parser.tokens.begin();
 	parser.stack_depth = 0;
 
-	create_scope( parser );
+	create_function( parser, "<global>", FunctionType::fn_global );
 
 	while ( !match( parser, token_eof ) ) {
 		declaration( parser );
 	}
 
-	destroy_scope( parser );
+	finish_function( parser );
 
-	emit( parser, OpCode::op_return );
-	printf( "emit op_return\n" );
+	program->functions = parser.functions;
+	program->global = 0;
 
-	return true;
+	auto main_function = std::find_if( program->functions.begin(), program->functions.end(), []( const Function& fn ) {
+		return fn.name == "Main";
+	} );
+
+	if ( main_function == program->functions.end() ) {
+		throw std::exception( "Missing 'Main' method" );
+	}
+
+	main_function->type = FunctionType::fn_main;
+	program->main = std::distance( program->functions.begin(), main_function );
 }
 
-#define arithmetic_op( op ) { auto b = stack.pop(); auto a = stack.pop(); stack.push( a op b ); }
+#define arithmetic_op( op ) { \
+	auto b = stack_pop( vm ); \
+	auto a = stack_pop( vm ); \
+	stack_push( vm, a op b ); \
+}
 
-template< typename V = double, typename C = std::stack< V > >
-class tb_stack {
-public:
-	V pop() {
-		auto value = container.top();
-		container.pop();
+struct VM {
+	struct Frame {
+		const uint32_t*		code;
+		const uint32_t*		ip;
+		double*				base;
+	};
 
-		return value;
-	}
-
-	void push( V value ) {
-		container.push( value );
-	}
-
-	V slot( int number ) {
-		return container._Get_container().at( number );
-	}
-private:
-	C container;
+	double*							stack;
+	double*							stack_top;
+	std::vector< Frame >			frames;
+	Program							program;
 };
 
-double execute( const std::vector< uint32_t >& code ) {
-	const uint32_t* base = code.data();
-	tb_stack< double > stack;
+double stack_pop( VM& vm ) {
+	--vm.stack_top;
 
-	for ( const uint32_t* ip = base;; ++ip ) {
+	if ( vm.stack_top < vm.stack ) {
+		throw std::exception( "Stack underflow" );
+	}
+
+	return *vm.stack_top;
+}
+
+double stack_push( VM& vm, double value ) {
+	*vm.stack_top = value;
+	++vm.stack_top;
+
+	if ( vm.stack_top - vm.stack >= 255 ) {
+		throw std::exception( "Maximum VM stack size exceeded" );
+	}
+}
+
+double execute( VM& vm, const Function& fn ) {
+	const uint32_t* code = fn.code.data();
+	double* base = vm.stack;
+
+	for ( const uint32_t* ip = code;; ++ip ) {
 		switch ( *ip ) {
 		case op_add: arithmetic_op( + ); break;
 		case op_sub: arithmetic_op( - ); break;
@@ -485,17 +657,56 @@ double execute( const std::vector< uint32_t >& code ) {
 			double result;
 			decode_double( result, part0, part1 );
 
-			stack.push( result );
+			stack_push( vm, result );
 			break;
 		}
-		case op_load_zero: stack.push( 0.0 ); break;
-		case op_load_slot: stack.push( stack.slot( *++ip ) ); break;
-		case op_pop: stack.pop(); break;
-		case op_return: return stack.pop();
+		case op_load_zero: stack_push( vm, 0.0 ); break;
+		case op_load_slot: stack_push( vm, base[ *++ip ] ); break;
+		case op_pop: stack_pop( vm ); break;
+		case op_return: {
+			if ( vm.frames.size() == 0 ) {
+				return stack_pop( vm );
+			}
+			
+			auto& return_frame = vm.frames[ vm.frames.size() - 1 ];
+
+			base = return_frame.base;
+			code = return_frame.code;
+			ip = return_frame.ip;
+
+			vm.frames.pop_back();
+			break;
+		}
+		case op_call: {
+			auto function_index = *++ip;
+			auto arg_count = *++ip;
+
+			vm.frames.push_back( VM::Frame{ code, ip, base } );
+
+			auto& function = vm.program.functions[ function_index ];
+
+			base = vm.stack_top - arg_count;
+			code = function.code.data();
+			ip = code - 1;
+			break;
+		}
 		default:
 			throw std::exception( ( "Invalid instruction '" + std::to_string( *ip ) + "'" ).c_str() );
 		}
 	}
+}
+
+double run( Program program ) {
+	VM vm;
+	vm.program = program;
+	vm.stack = new double[ 255 ];
+	vm.stack_top = &vm.stack[ 0 ];
+
+	execute( vm, vm.program.functions[ vm.program.global ] );
+	auto return_value = execute( vm, vm.program.functions[ vm.program.main ] );
+
+	delete vm.stack;
+	return return_value;
 }
 
 int main( int argc, char** argv ) {
@@ -516,14 +727,16 @@ int main( int argc, char** argv ) {
 
 			printf( "========== Compiler ==========\n" );
 
-			std::vector< uint32_t > code;
-			parse( tokens, &code );
+			Program program;
+			parse( tokens, &program );
 
-			std::cout << "size of code: " << code.size() << std::endl;
+			std::cout << "# of functions " << program.functions.size() << std::endl;
+			std::cout << "size of code (global scope): " << program.functions[ program.global ].code.size() << std::endl;
+			std::cout << "size of code (Main): " << program.functions[ program.main ].code.size() << std::endl;
 
 			printf( "========== Execution (VM) ==========\n" );
 
-			auto result = execute( code );
+			auto result = run( program );
 			printf( "Return: %f\n", result );
 		} catch ( const std::exception& err ) {
 			printf( "Error: %s\n", err.what() );
